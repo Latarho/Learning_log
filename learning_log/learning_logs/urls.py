505fa9f01166b4ff0a8163996f1f2c5e66abb7d5
latarho <latarho2@gmail.com>
2021-06-18 22:30:36 +0300

    Страница со списком тем.

--- learning_log/learning_logs/urls.py
+++ learning_log/learning_logs/urls.py
@@ -4,6 +4,8 @@ from . import views
 
 app_name = 'learning_logs'
 urlpatterns = [
-    # Домашняя страница
+    # Домашняя страница.
     path('', views.index, name='index'),
+    # Страница со списком всех тем.
+    path('topics/', views.topics, name='topics'),
 ]
